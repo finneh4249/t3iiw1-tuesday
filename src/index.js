d1c78fc2074cc378b4839006c7ab9eeb57774c60
Ethan Cornwill <ethancornwill@outlook.com>
2025-01-16 20:13:42 +1100

    Add dotenv dependency and configure environment variables

--- src/index.js
+++ src/index.js
@@ -6,10 +6,13 @@
 // Tell the server to listen on the port for incoming requests
 
 // Server is configured in this file
+require('dotenv').config()
 const { app } = require('./server')
+const { dbConnect } = require("./utils/database.js");
 
 const PORT = process.env.PORT ?? 3000
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
+    await dbConnect();
     console.log(`Server is running on http://localhost:${PORT}`)
 })
